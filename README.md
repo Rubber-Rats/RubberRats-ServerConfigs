f95c1f2fc78fbcca7f365497768bd2ee644e1ccf
Mutable Figment <147920149+mutablefigment@users.noreply.github.com>
2024-06-24 11:21:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rubber Rats server config
+
+Nixos config for the server
+
+
+Rebuild the server with this command while logged in via ssh:
+
+```sh
+sudo nixos-rebuild swtich
+```
---
+++ serverconfig/configuration.nix
@@ -0,0 +1,60 @@
+{ ... }: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+  networking.hostName = "www";
+  networking.domain = "rubberroomwithrats.com";
+
+  networking.firewall.enable = false;
+
+  #TODO autofetch the keys from our github profiles
+
+  services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = [
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFecTaDusQuqN5tWYitmFMqZEbBMeZA8SWLKOtlLE5tI psychopathy@psychopathy-desktop''
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5432hWcdfQX8icuV1W4xV63NB9uQ4yUaJztTXX5qdT''
+  ];
+
+
+  #PROSODY SERVER
+
+  #NGINX
+  services.nginx = {
+    enable = true;
+   #Recomended settings
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    
+    virtualHosts."localhost" = {
+
+      root = "/var/www/www.rubberroomwithrats.com";
+
+    };
+  };
+ 
+  # acme setup
+  security.acme = {
+    email = "root@rubberroomwithrats.com";
+    acceptTerms = true;
+    certs = {
+      "rubberroomwithrats.com" = {
+       webroot = "/var/www/www.rubberroomwithrats.com";
+       email = "root@rubberroomwithrats.com";
+       extraDomainNames = [ 
+        "xmpp.rubberroomwithrats.com" 
+        "conference.rubberroomwithrats.com" 
+        "upload.rubberroomwithrats.com" 
+        "www.rubberroomwithrats.com" 
+       ];
+      };
+    };
+  };
+
+ 
+  system.stateVersion = "23.11";
+}
